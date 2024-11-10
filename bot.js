9fbde91dde0f15e839c58be86ce4e83ef86b6e2b
vornisoe <99256040+vornisoe@users.noreply.github.com>
2024-11-10 19:05:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,8 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('ready', () => {
-    console.log('Bot jest online!'); // Pl: Chcesz to zmień sobie napis na inny, EN: You can change the text to another one if you want.
+    console.log('Bot jest online!'); // Pl: Chcesz to zmień sobie napis na inny
 });
 
 //PL: Nie wpisujemy tutaj naszego tokenu, token jest przechowywany w pliku token.env
-//EN: We do not enter our token here; the token is stored in the token.env file.
 client.login(process.env.DISCORD_TOKEN);
